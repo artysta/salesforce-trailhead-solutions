a31cce29d45a252bdb3a2bdbdc5e09b6f550d3d2
Adrian Kurek <adrian.kurek93@gmail.com>
2021-08-13 13:09:35 +0200

    Add solution for "Work with Lists of Records"

---
+++ Build a Bear-Tracking App with Lightning Web Components/Work with Lists of Records/build-apps-with-lwc/force-app/main/default/lwc/bearList/bearList.js
@@ -0,0 +1,26 @@
+import { LightningElement, wire } from 'lwc';
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
+export default class BearList extends LightningElement {
+    searchTerm = '';
+    @wire(searchBears, { searchTerm: '$searchTerm' })
+    bears;
+    appResources = {
+        bearSilhouette: `${ursusResources}/img/standing-bear-silhouette.png`,
+    };
+    handleSearchTermChange(event) {
+        // Debouncing this method: do not update the reactive property as
+        // long as this function is being called within a delay of 300 ms.
+        // This is to avoid a very large number of Apex method calls.
+        window.clearTimeout(this.delayTimeout);
+        const searchTerm = event.target.value;
+        // eslint-disable-next-line @lwc/lwc/no-async-operation
+        this.delayTimeout = setTimeout(() => {
+            this.searchTerm = searchTerm;
+        }, 300);
+    }
+    get hasResults() {
+        return (this.bears.data.length > 0);
+    }
+}
