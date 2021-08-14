dd4a290bc25267fc4d856571fe14796367d69ba8
Adrian Kurek <adrian.kurek93@gmail.com>
2021-08-14 20:46:39 +0200

    Add solution for "Create a Child Component and Interact with It"

---
+++ Build a Bear-Tracking App with Lightning Web Components/Create a Child Component and Interact with It/build-apps-with-lwc/force-app/main/default/lwc/helloWebComponent/helloWebComponent.js
@@ -0,0 +1,14 @@
+import { LightningElement } from 'lwc';
+
+export default class HelloWebComponent extends LightningElement {
+    greeting = 'Trailblazer';
+
+    currentDate = new Date().toDateString();
+    get capitalizedGreeting() {
+        return `Hello ${this.greeting.toUpperCase()}!`;
+    }
+
+    handleGreetingChange(event) {
+        this.greeting = event.target.value;
+    }
+}
