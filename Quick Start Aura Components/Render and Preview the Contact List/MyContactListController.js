a9d5013d4fc37251a730d02d494308f298d46d92
Adrian Kurek <adrian.kurek93@gmail.com>
2021-08-30 11:24:29 +0200

    Add solution for "Render and Preview the Contact List"

---
+++ Quick Start Aura Components/Render and Preview the Contact List/MyContactListController.js
@@ -0,0 +1,17 @@
+({
+    myAction : function(component, event, helper) {
+        component.set("v.Columns", [
+            {label:"First Name", fieldName:"FirstName", type:"text"},
+            {label:"Last Name", fieldName:"LastName", type:"text"},
+            {label:"Phone", fieldName:"Phone", type:"phone"}
+        ]);
+        var action = component.get("c.getContacts");
+        action.setParams({
+            recordId: component.get("v.recordId")
+        });
+        action.setCallback(this, function(data) {
+            component.set("v.Contacts", data.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    }
+})
