beada6925cbdb683fa189d119555c5b8d3c8c157
Adrian Kurek <adrian.kurek93@gmail.com>
2021-11-01 10:13:38 +0100

    Add solution for "Write Your First SOQL Query"

---
+++ Transform SQL Queries to SOQL Queries in a Lightning App/Write Your First SOQL Query/Books4EveryoneHomeController.js
@@ -0,0 +1,10 @@
+({
+    doInit: function (component, event, helper) {
+        var action = component.get("c.getBooks");
+        action.setCallback(this, function (data) {
+            component.set("v.Books", data.getReturnValue());
+            console.log(data.getReturnValue());
+        });
+        $A.enqueueAction(action);
+    }
+})
