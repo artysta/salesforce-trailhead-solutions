1973de35b716bfb5ac1ae0ddbd61554873d3e07e
Adrian Kurek <adrian.kurek93@gmail.com>
2025-04-28 10:48:19 +0200

    Add solution for "Create a Reactive Screen"

---
+++ Quick Start Build Reactive Screen Flows/Create a Reactive Screen/.sfdx/typings/lwc/user.d.ts
@@ -0,0 +1,15 @@
+/**
+ * User's Id.
+ */
+declare module '@salesforce/user/Id' {
+    const id: string;
+    export default id;
+}
+
+/**
+ * User's is guest status.
+ */
+declare module '@salesforce/user/isGuest' {
+    const isGuest: boolean;
+    export default isGuest;
+}
